317913698f888ac0ec3fc1b06b6f8f066c8ed884
mabdali <muslumabdali1234@gmail.com>
2023-07-29 16:24:16 -0400

    Fixed directory issues.

--- autoLocke/Functions/ImageGrab.py
+++ autoLocke/Functions/ImageGrab.py
@@ -1,6 +1,7 @@
 import pyautogui
 from autoLocke.Files.Data.CordsDict import routeCordDictionary, caughtRouteDictionary
 
+
 class screenshotClass:
     def __init__(self):
         self.routeDict = routeCordDictionary
@@ -10,16 +11,15 @@ class screenshotClass:
     # Requires the section (Route or Caught), and Generation because different games have different ss cords.
     # Takes the section and assigns it the appropriate list from routeCordDictionary or caughtRouteDictionary
     def takeSection(self, section, gen):
-        global filepath, x,y,width,height
         routeDictGen = self.routeDict[gen]
-        x,y,width,height = routeDictGen[0], routeDictGen[1], routeDictGen[2], routeDictGen[3]
+        x, y, width, height = routeDictGen[0], routeDictGen[1], routeDictGen[2], routeDictGen[3]
         if section == "Route":
             filepath = "autoLocke/Files/Images/RouteImage.png"
-            self.takeScreenshot(section=section, gen=gen)
+            self.takeScreenshot(filepath, x, y, width, height)
         else:
             filepath = "autoLocke/Files/Images/CaughtImage.png"
 
-    def takeScreenshot(self):
+    def takeScreenshot(self, filepath, x, y, width, height):
         screenshot = pyautogui.screenshot(region=(x,y,width,height))
         screenshot.save(filepath)
 
