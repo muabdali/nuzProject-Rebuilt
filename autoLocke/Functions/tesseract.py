826b47c198c64ab23972badcdf884dfb61ccf038
mabdali <muslumabdali1234@gmail.com>
2023-07-29 17:21:16 -0400

    Started working on image processing

---
+++ autoLocke/Functions/tesseract.py
@@ -0,0 +1,8 @@
+import pytesseract
+
+# Python file dedicated to enhancing, reading, and returning given images.
+
+class imgProcess():
+    def __init__(self):
+        return
+    def imageEnhance(self, image):
