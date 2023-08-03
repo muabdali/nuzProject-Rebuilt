e15621e8a873fa910c6b50bb5c2ef3019824c6a5
mabdali <muslumabdali1234@gmail.com>
2023-08-03 19:49:11 -0400

    Started work on pytesseract img to string

--- autoLocke/Functions/tesseract.py
+++ autoLocke/Functions/tesseract.py
@@ -1,9 +1,15 @@
 import pytesseract
+import cv2
 
 # Python file dedicated to enhancing, reading, and returning given images.
 
 class imgProcess():
     def __init__(self):
         return
-    def imageEnhance(self, image):
-        
+    def imageEnhance(self, imagePath):
+        img = cv2.imread(imagePath)
+        img = cv2.resize(img, None, fx=1.2, fy=1.2, interpolation=cv2.INTER_CUBIC)
+        print(pytesseract.image_to_string(img))
+
+ia = imgProcess()
+ia.imageEnhance(imagePath='autoLocke/Files/Images/ocrTests/PalletTown.png')
