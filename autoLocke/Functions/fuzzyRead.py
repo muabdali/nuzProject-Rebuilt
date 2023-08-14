65ed55120d7b9d06a5cfecc990fe135f58fc5a7c
Muslum Abd Ali <muslumabdali1234@gmail.com>
2023-08-14 05:19:12 -0400

    Description on fuzzyRead

--- autoLocke/Functions/fuzzyRead.py
+++ autoLocke/Functions/fuzzyRead.py
@@ -10,3 +10,19 @@ class fuzzChecker:
             return None
         else:
             return best_match
+        return
+    
+
+        """
+        This file contains a single class with a single function.
+        The function utilizes fuzzywuzzy to compare the scanned text to the desired results.
+        
+        Due to the inherent inaccuracy in pytesseract, sometimes it may give an incorrect
+        string, for example, instead of Route 1, it will give Roote 1. FuzzyChecker will take
+        "Roote 1", compare it to all the route names, and return the proper name, in this case
+        "Route 1". this is vital as the for loop that checks for route names ONLY recognises correctly
+        spelt names.
+        
+        In the future, this should be updated to the new fuzzywuzzy library named "thefuzz", but for now it 
+        should do the job.
+        """
