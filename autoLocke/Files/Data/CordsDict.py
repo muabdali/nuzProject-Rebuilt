e3796757ce8303d4074287634f4c0b39a2a0a124
Muslum Abd Ali <muslumabdali1234@gmail.com>
2023-08-14 05:05:34 -0400

    Added description to CordsDict.py

--- autoLocke/Files/Data/CordsDict.py
+++ autoLocke/Files/Data/CordsDict.py
@@ -11,3 +11,19 @@ caughtRouteDictionary = {
 
 }
 
+
+
+"""
+File holding the dictionaries that specify the coordinates 
+that pyautogui requires to take screenshots.
+
+the order goes (in pixels)
+
+x, y, width, height 
+
+
+caughtRoute is for capturing the caught condition (bottom left), the routeCord
+is the Route area (top left)
+
+
+"""
