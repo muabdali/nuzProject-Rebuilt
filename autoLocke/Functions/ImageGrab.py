55cc73caf196332d574ab32329ccd0313e2c639a
Muslum Abd Ali <muslumabdali1234@gmail.com>
2023-08-17 02:08:58 -0400

    Added order of operations for ImageGrab

--- autoLocke/Functions/ImageGrab.py
+++ autoLocke/Functions/ImageGrab.py
@@ -25,31 +25,34 @@ class screenshotClass:
             filepath = "autoLocke/Files/Images/RouteImage.png"
             result = self.takeScreenshot(filepath, x, y, width, height)
             print(result)
+            
         else:
             filepath = "autoLocke/Files/Images/CaughtImage.png"
-            result = self.takeScreenshot(filepath, x,y,width,height)
+            result = self.takeScreenshot(filepath, x,y,width,height,section=section)
             print(result)
 
-    def takeScreenshot(self, filepath, x, y, width, height):
+    def takeScreenshot(self, filepath, x, y, width, height, section):
         screenshot = pyautogui.screenshot(region=(x,y,width,height))
         screenshot.save(filepath)
-        result = self.tessRead(path=filepath, section="")
+        result = self.tessRead(path=filepath, section=section)
         return result
     
     def tessRead(self, path, section):
         tesseractRead = imgProcess()
         tesseractResult = tesseractRead.imageEnhance(imagePath=path)
         fuzzyResult = self.fuzzyCompare(text=tesseractResult, section=section)
+        return fuzzyResult
         
+    # DOES NOT NEED TO BE CALLED SEPERATELY, ALREADY CALLED FROM tessRead.
     def fuzzyCompare(self, text, section):
         fuzzProcess = fuzzChecker()
         if section == "Route":
             tocheckList = "autoLocke/Files/Data/fireredroutes.txt"
-            fuzzProcess.checkList(pokeList=tocheckList,nameToCheck=text,minScore=85)
+            fuzzProduct = fuzzProcess.checkList(pokeList=tocheckList,nameToCheck=text,minScore=85)
         elif section == "Caught":
             tocheckList = "autoLocke/Files/Data/NatDexPokemonG3.txt"
-            fuzzProcess.checkList(pokeList=tocheckList,nameToCheck=text, minScore=85)
-
+            fuzzProduct = fuzzProcess.checkList(pokeList=tocheckList,nameToCheck=text, minScore=85)
+        return fuzzProduct
     def executeFunction(self, section, gen):
         self.takeSection(section, gen)
 
@@ -75,4 +78,25 @@ fuzzyRead is a vital function for this program. Since tesseract is innacurate, t
 the image_to_text result. FuzzyRead counters this by comparing the given text with a list of expected strings, and choosing the
 closest one. 
 
+
+ORDER OF OPERATION
+
+executeFunction is called.
+executeFunction calls takeSection(section, gen)
+takeSection() determines the appropriate screenshot dimensions depending on the generation and section. Also determines the filepath.
+takeSection() calls takeScreenshot(filepath, dimensions)
+
+takeScreenshot takes a screenshot given the dimensions and saves it to the given filepath
+takeScreenshot then takes the filepath, and feeds it into tessRead(section, gen)
+
+
+tessRead uses the Tesseract-OCR to get a general string given the path.
+tessRead then calls fuzzyCompare(text, section)
+
+fuzzyCompare takes the text and compares it to the given section's path.
+fuzzyCompare returns the proper text if it finds a match.
+
+The code is then called all the way back and is returned to takeSection with the matching
+string.
 """
+
